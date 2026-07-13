f44791fbd12b09745df0aaec3c88879a5bdf130e
Moses Wong <moseswong0728@gmail.com>
2026-07-13 17:33:23 +0800

    Add POST /expenses endpoint

--- backend/code/index.js
+++ backend/code/index.js
@@ -3,7 +3,9 @@ const path = require("path");
 const fs = require("fs").promises;
 const app = express();
 const port = 3000;
-const expensesFile = path.join(__dirname, "../files/expenses.json")
+const expensesFilePath = path.join(__dirname, "../files/expenses.json")
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({
@@ -12,14 +14,26 @@ app.get("/", (req, res) => {
 });
 
 app.get("/expenses", (req, res) => {
-    res.sendFile(expensesFile);
+    res.sendFile(expensesFilePath);
 });
 
 app.get("/expenses/:id", async (req, res) => {
     const targetId = Number(req.params.id);
-    const expenses = JSON.parse(await fs.readFile(expensesFile, "utf-8"));
+    const expenses = JSON.parse(await fs.readFile(expensesFilePath, "utf-8"));
     const expense = expenses.find(item => item.id === targetId);
     res.json(expense);
+});
+
+app.post("/expenses", async (req, res) => {
+    const expenses = JSON.parse(await fs.readFile(expensesFilePath, "utf-8"));
+    const newExpense = req.body;
+    newExpense.id = expenses.at(-1).id + 1;
+    expenses.push(newExpense);
+    await fs.writeFile(expensesFilePath, JSON.stringify(expenses, null, 2));
+    res.json({
+        "message": "Expense created successfully!",
+        "expense": newExpense
+    })
 });
 
 app.listen(port, () => {
