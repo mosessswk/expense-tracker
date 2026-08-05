7fb8c134cbed2726133cca62ea68fc2310c0b09d
Moses Wong <moseswong0728@gmail.com>
2026-08-05 12:36:04 +0800

    chore: add frontend build and secrets

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT;
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:4173"],
     credentials: true
 }));
 
